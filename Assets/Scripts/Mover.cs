7f478aade1d9ea8c90d4714ba95e6f0c7b4373e3
Alex Swehla <alex@thinair.com>
2016-05-23 21:57:35 -0700

    PEW PEW LASERS!

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    public float speed;
+
+    void Start() {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,21 @@ public class PlayerController : MonoBehaviour {
     
     public float speed;
     public float tilt;
+    public float fireRate;
     public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    
+    private float nextFire;
+
+
+    void Update() {
+        if ( Input.GetButton ("Fire1") && Time.time > nextFire ) {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position,shotSpawn.rotation);
+        }
+    }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis ("Horizontal");
